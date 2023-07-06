60bd8efbbaf29d6ddbe6a192fe2e1ed2e71c3001
Joe Kelly <joedotkelly@gmail.com>
2023-07-06 21:40:54 +0100

    set page title to first message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,23 @@
-<script>    
+<script>
+    import { messages } from '$lib/stores/chat'
     import Header from '$lib/Header.svelte'
     import Chat from '$lib/Chat.svelte'
     import Input from '$lib/Input.svelte'
 
     let chat
+    let title
+
+    $: {
+        if ($messages.length > 1) {
+            title = $messages[1].content
+        } else {
+            title = 'Svelte GPT'
+        }
+    }
 </script>
 
 <svelte:head>
-    <title>Svelte GPT</title>
+    <title>{title}</title>
     <link rel='stylesheet' href='https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.8.0/build/styles/github-dark.min.css'>
 </svelte:head>
 
