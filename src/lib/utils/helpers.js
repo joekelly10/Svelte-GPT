b0d36ccd079d4b80697805286bdc8cf7d99e9919
Joe Kelly <joedotkelly@gmail.com>
2023-07-06 10:42:14 +0100

    update filename for saved chats

--- src/lib/utils/helpers.js
+++ src/lib/utils/helpers.js
@@ -7,7 +7,7 @@ export const getDateString = () => {
     const minutes = String(now.getMinutes()).padStart(2,'0')
     const seconds = String(now.getSeconds()).padStart(2,'0')
   
-    return `${year}-${month}-${day}-${hours}.${minutes}.${seconds}`
+    return `${year}-${month}-${day}@${hours}.${minutes}.${seconds}`
 }
 
 export const isStreamedChatCompletion = (data) => {
