3ee5e8b08874bdb301051c52fba30e119bb7c159
Joe Kelly <joedotkelly@gmail.com>
2024-10-28 10:25:28 +0000

    set default prompt active in db if active prompt was deleted

--- src/routes/api/system-prompts/[id]/+server.js
+++ src/routes/api/system-prompts/[id]/+server.js
@@ -4,7 +4,14 @@ import { POCKETBASE_URL } from '$lib/config'
 
 export const DELETE = async ({ params }) => {
     try {
-        const pb   = new PocketBase(POCKETBASE_URL)
+        const pb     = new PocketBase(POCKETBASE_URL)
+        const active = await pb.collection('system_prompts').getFirstListItem('active=true')
+
+        if (params.id === active.id) {
+            const default_prompt = await pb.collection('system_prompts').getFirstListItem('default=true')
+            await pb.collection('system_prompts').update(default_prompt.id, { active: true })
+        }
+
         const data = await pb.collection('system_prompts').delete(params.id)
 
         return json(data, { status: 204 })
