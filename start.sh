d5c8dd0ca1edabce937e931aacdc0b36a1e2879a
Joe Kelly <joedotkelly@gmail.com>
2024-10-30 07:57:29 +0000

    add logo in start script

--- start.sh
+++ start.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 # Colors
-yellow='\033[0;33m'
 blue='\033[0;34m'
 cyan='\033[0;36m'
 white='\033[0;37m'
-yellow_bold='\033[1;33m'
 blue_bold='\033[1;34m'
 cyan_bold='\033[1;36m'
 white_bold='\033[1;37m'
@@ -15,14 +13,22 @@ reset='\033[0m'
 APP_PORT=$(grep "const APP_PORT" ./src/lib/config.js | sed "s/.*APP_PORT *=* *//;s/[',]//g" | tr -d '[:space:]')
 POCKETBASE_PORT=$(grep "const POCKETBASE_PORT" ./src/lib/config.js | sed "s/.*POCKETBASE_PORT *=* *//;s/[',]//g" | tr -d '[:space:]')
 
+echo -e "\n\n"
+echo -e "${white_bold} .d8888b.  888     888 8888888888 888    88888888888 8888888888       .d8888b.  8888888b. 88888888888                 ${blue_bold}d88P   d88P${reset}"
+echo -e "${white_bold}d88P  Y88b 888     888 888        888        888     888             d88P  Y88b 888   Y88b    888                    ${blue_bold}d88P   d88P${reset}"
+echo -e "${white_bold}Y88b.      888     888 888        888        888     888             888    888 888    888    888                   ${blue_bold}d88P   d88P${reset}"
+echo -e "${white_bold} \"Y888b.   Y88b   d88P 8888888    888        888     8888888         888        888   d88P    888                  ${blue_bold}d88P   d88P${reset}"
+echo -e "${white_bold}    \"Y88b.  Y88b d88P  888        888        888     888             888  88888 8888888P\"     888                 ${blue_bold}d88P   d88P${reset}"
+echo -e "${white_bold}      \"888   Y88o88P   888        888        888     888             888    888 888           888                ${blue_bold}d88P   d88P${reset}"
+echo -e "${white_bold}Y88b  d88P    Y888P    888        888        888     888             Y88b  d88P 888           888               ${blue_bold}d88P   d88P${reset}"
+echo -e "${white_bold} \"Y8888P\"      Y8P     8888888888 88888888   888     8888888888       \"Y8888P88 888           888              ${blue_bold}d88P   d88P${reset}"
+echo -e "\n\n"
+
 # Start Pocketbase in the background
 pocketbase/pocketbase serve --http="localhost:${POCKETBASE_PORT}" > pocketbase/pocketbase.log &
 
-echo -e "\n\n"
 echo -e "  ${blue_bold}POCKETBASE${reset}\n"
-echo -e "  ${blue_bold}➜ ${white_bold}Local: ${cyan}http://localhost:${cyan_bold}${POCKETBASE_PORT}${cyan}/${reset}"
-echo -e "  ${blue_bold}➜ ${white_bold}Admin: ${yellow}http://localhost:${yellow_bold}${POCKETBASE_PORT}${yellow}/_/${reset}"
-echo -e "\n"
+echo -e "  ${blue_bold}➜ ${white_bold}Admin: ${blue}http://localhost:${blue_bold}${POCKETBASE_PORT}${blue}/_/${reset}\n"
 
 # Store the PID of the Pocketbase process
 pocketbase_pid=$!
